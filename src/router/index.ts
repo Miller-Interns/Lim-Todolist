8dd9d90b4c3b01f22f99f86940da1135f8dc8b38
SephJL <joseph.libron.lim@gmail.com>
2025-01-28 11:53:37 +0800

    refactor: remove white space, change icons, and adjust code

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/todoListView',
+      path: '/todo',
       name: 'todoListView',
       component: TodoListView,
     },
